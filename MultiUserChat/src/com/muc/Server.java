5312586edddea245ad13b66c465b6c1a36b411bc
WildCanine <matthew.woodall9090@gmail.com>
2019-05-28 11:38:25 -0600

    Initial Commit

--- MultiUserChat/src/com/muc/Server.java
+++ MultiUserChat/src/com/muc/Server.java
@@ -31,4 +31,7 @@ public class Server extends Thread {
             e.printStackTrace();
         }
     }
+    public void removeWorker(ServerWorker serverWorker) {
+        workerList.remove(serverWorker);
+    }
 }
